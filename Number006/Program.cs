5b41bdf5dba4a74d08d62d86e2e704b506f9a9c5
Vadim <orc225@mail.ru>
2023-01-14 21:06:53 +0300

    initial commit

---
+++ Number006/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите Ваше число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost = a % 2;
+if (ost == 0)   
+Console.Write("Введенное число четное");
+else
+Console.Write("Введенное число нечетное");
