a68e8b25a8f175e23ed4837861f0796d39418677
Vadim <orc225@mail.ru>
2023-01-14 20:53:42 +0300

    Initial commit

---
+++ Number004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+max = b;
+if (c > max)   
+max = c;
+
+Console.Write($"Максимальное число: {max}");
