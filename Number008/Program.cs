e93f48c566c349b43e712dd05f5669c080901703
Vadim <orc225@mail.ru>
2023-01-14 21:32:23 +0300

    initial commit

---
+++ Number008/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 2;   
+while (count <= a) 
+{
+if (count < a-1)
+Console.Write($"{count}, ");
+else
+Console.Write($"{count}");
+count = count + 2;
+}
