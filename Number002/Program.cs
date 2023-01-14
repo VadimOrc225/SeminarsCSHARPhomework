7d128c22bcc25b66d8a8c9c6f0810829cf6426d0
Vadim <orc225@mail.ru>
2023-01-14 20:31:26 +0300

    Initial commit

---
+++ Number002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)   
+Console.Write($"max = {a}");
+if (b > a)
+Console.Write($"max = {b}");
+if (b == a)
+Console.Write("Введенные числа равны");
