e7efa02b07464040e2ec78b75a0933b64b52e1fb
Vadim <orc225@mail.ru>
2023-01-21 22:52:02 +0300

    После третьего семинара initial commit

---
+++ Seminar3/Number 23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= N)
+{
+    int rezult = Convert.ToInt32(Math.Pow(count,3));
+    Console.WriteLine(rezult);
+    count++;
+}
