c61e8a6d72d1ac0443862db25cc16564028e27d2
Vadim <orc225@mail.ru>
2023-01-22 16:21:07 +0300

    initial commit

---
+++ Seminar3/Sem4-2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int Sum(int A)
+{
+   
+    int sum = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        sum = sum + i ;
+    }
+    return sum;
+}
+Console.Write("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{A} -> {Sum(A)}");
+
