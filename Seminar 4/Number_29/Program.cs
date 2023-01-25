8d52259c48f111e8b7f574a5ba16b93abb27ae16
Vadim <orc225@mail.ru>
2023-01-25 22:59:46 +0300

    Вывод случайного массива из 8 элементов

---
+++ Seminar 4/Number_29/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[8];
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Zapolnenie(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+}
+
+Zapolnenie(arr);
+PrintArray(arr);
