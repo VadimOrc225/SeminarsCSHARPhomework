4d0642dfb56e02cb5b2c67f19f7c31097c64e908
Vadim <orc225@mail.ru>
2023-01-25 23:19:16 +0300

    Степень числа

---
+++ Seminar 4/Number_25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите основание степени ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите показатель степени ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int z = N;
+while (count<a)
+{
+z = z * N;
+count++;
+}
+Console.WriteLine($"Ответ: {z}");
