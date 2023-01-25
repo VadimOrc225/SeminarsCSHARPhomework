22cbc46222219cb94184f5535361ff0acc70e6b2
Vadim <orc225@mail.ru>
2023-01-25 23:10:32 +0300

    Сумма цифр числа

---
+++ Seminar 4/Number_27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 10;
+int x = N % 10;
+int sum = 0;
+while (N / count != 0)
+{
+sum = sum + (N / count) % 10;
+count = count * 10;
+}
+sum = sum + x;
+Console.WriteLine($"Сумма цифр числа равна {sum}");
