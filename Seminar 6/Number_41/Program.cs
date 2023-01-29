fee7b309e3508bd1a1d0e617f2388278a4249fac
Vadim <orc225@mail.ru>
2023-01-29 22:32:49 +0300

    После семинара 6

---
+++ Seminar 6/Number_41/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите массив чисел через запятую");
+string s = Console.ReadLine();
+string[] nums = s.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+int[] n = nums.Select(Int32.Parse).ToArray();
+int sum = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0)
+    {
+        sum = sum + n[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных элементов равна {sum}");
