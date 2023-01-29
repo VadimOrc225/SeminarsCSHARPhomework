7cef2644647ae6366a06352b5762e14435374ade
Vadim <orc225@mail.ru>
2023-01-29 16:18:26 +0300

    sem6

---
+++ Seminar 4/Number 40/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+
+Console.WriteLine("Ведите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите первое число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите первое число ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a<b+c&&b<a+c&&c<a+b)
+Console.WriteLine("Треуг сущ ");
+else
+Console.WriteLine("Треуг не сущ ");
