bd5d2dfa3e8597c721d8c2823276422478a3bcdc
Vadim <orc225@mail.ru>
2023-02-05 10:13:15 +0300

    Заполнение массива вещественными числами

---
+++ Seminar 7/Number 47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 47. Задайте двумерный массив размером mxn, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+double[,] array = new double[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = (Convert.ToDouble(new Random().Next(-1000, 1000)))/100;
+        
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine();
