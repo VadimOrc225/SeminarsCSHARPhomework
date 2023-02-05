0e1bfd343cd62143f8422791b80995fde1cd73b3
Vadim <orc225@mail.ru>
2023-02-05 09:40:39 +0300

    sem7

---
+++ Seminar 6/Number 46/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(-10, 100);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine();
