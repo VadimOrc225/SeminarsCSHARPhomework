414d045881ab27c5ff1f50f7b37729c1b6a2b416
Vadim <orc225@mail.ru>
2023-02-05 09:42:32 +0300

    доп задача вариант 2

---
+++ Seminar 6/Number 51/Program.cs
@@ -0,0 +1,9 @@
+﻿for (int x5 = 5; x5 < 300; x5++)
+{
+    int x4 = x5 - 4, x3 = x5 - 3, x2 = x5 - 2, x1 = x5 - 1;
+    if (Convert.ToInt64((Math.Pow(x1, 5)) + (Math.Pow(x2, 5)) + (Math.Pow(x3, 5)) + (Math.Pow(x4, 5))) == Convert.ToInt64(Math.Pow(x5, 5)))
+        Console.WriteLine($"x1 = {x1}, x2 = {x2}, x3 = {x3}, x4 = {x4}, x5 = {x5}");
+
+}
+
+
