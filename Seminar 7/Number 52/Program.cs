c1f8643a977b5e2366f12decbc99e2dbab5d2bc6
Vadim <orc225@mail.ru>
2023-02-05 11:16:10 +0300

    Среднее арифметическое по столбцам

---
+++ Seminar 7/Number 52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+double[] array_sum = new double[n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        array_sum[j] = 0;
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.Write(" Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array_sum[j] = array_sum[j]+array[i,j];
+    }
+        array_sum[j] = Math.Round((array_sum[j]/array.GetLength(0)),2);
+        
+        Console.Write($"{array_sum[j]} ");
+        
+}
+    Console.WriteLine();
+
