f93ad73c97848c47b29893cb1c631b84a546071e
Vadim <orc225@mail.ru>
2023-02-05 09:53:38 +0300

    sem6 квадраты элементов массива с четными индексами

---
+++ Seminar 6/Number49/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+        {
+            int sqr = Convert.ToInt32(Math.Pow(array[i, j], 2));
+            array[i, j] = sqr;
+        }
+        Console.Write($"{array[i, j]} ");
+        
+    }
+    Console.WriteLine();
+}
