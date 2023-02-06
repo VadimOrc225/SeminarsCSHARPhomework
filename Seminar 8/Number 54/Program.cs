6130c9bb0b97cf14c9dcf296641efa43a790621e
Vadim <orc225@mail.ru>
2023-02-07 00:14:57 +0300

    Упорядочение по убыванию

--- Seminar 8/Number 54/Program.cs
+++ Seminar 8/Number 54/Program.cs
@@ -18,7 +18,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(0, 10);
+        array[i, j] = new Random().Next(0, 15);
         Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
