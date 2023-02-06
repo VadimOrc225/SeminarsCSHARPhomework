b7b121e5c1c0c3179f4d41dc05fda2108832f914
Vadim <orc225@mail.ru>
2023-02-06 21:50:38 +0300

    Частотный словарь с методом поиска элемента в массиве

---
+++ Seminar 6/number 57/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Набор данных
+// Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+int SearchElement(int[,] array, int element)
+{
+    int count = 0;
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            if (array[x, y] == element)
+                count = count + 1;
+        }
+    }
+    return count;
+}
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите верхнюю границу частотного словаря: ");
+int top = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, top);
+        Console.Write($"{array[i, j]} ");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < top; i++)
+{
+    int count = SearchElement(array, i);
+    if (count != 0)
+        Console.WriteLine($"{i} встречается {count} раз");
+}
+
