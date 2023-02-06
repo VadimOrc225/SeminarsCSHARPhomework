db4fc29e2f0126a1a15502f6be76a0385ef79ce9
Vadim <orc225@mail.ru>
2023-02-06 22:00:37 +0300

    Перевод из десятичной системы в двоичную

---
+++ Seminar 6/Translation/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числo");
+int num = Convert.ToInt32(Console.ReadLine());
+string s = "";
+while(num>0)
+{
+s = Convert.ToString(num%2) + s;
+num/=2;
+}
+
+Console.WriteLine($"В двоичном виде {s}");
